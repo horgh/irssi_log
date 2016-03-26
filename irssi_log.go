803e7433af1c834831b6bb81f80675abbd1d5113
Will Storey <will@summercat.com>
2016-03-26 13:16:35 -0700

    Parse several more line types
    Not yet all

--- irssi_log.go
+++ irssi_log.go
@@ -11,6 +11,7 @@ import (
 	"log"
 	"os"
 	"regexp"
+	"strconv"
 	"time"
 )
 
@@ -24,16 +25,38 @@ const (
 	JoinSync
 	Message
 	Quit
+	NickChange
 	DayChange
+	LogClosed
+	NowTalking
+	Emote
+	Topic
+	Kick
+	Part
+	YourNickChange
 )
 
 type LogEntry struct {
-	Time      time.Time
+	// Raw line
+	Line string
+
+	// Parsed time
+	Time time.Time
+
+	// Type of line
 	EntryType EntryType
-	Channel   string
-	Nick      string
-	UserHost  string
-	Text      string
+
+	// Channel, if available
+	Channel string
+
+	// Nick, if available
+	Nick string
+
+	// user@host, if available
+	UserHost string
+
+	// Text, if applicable. e.g., message text
+	Text string
 }
 
 func main() {
@@ -107,6 +130,8 @@ func parseLog(file *os.File, lineLimit int, location *time.Location) (
 
 		log.Printf("Parsed line %q", entry)
 
+		entries = append(entries, entry)
+
 		if entry.EntryType == LogOpen || entry.EntryType == DayChange {
 			currentDate = time.Date(entry.Time.Year(), entry.Time.Month(), entry.Time.Day(), 0, 0, 0, 0, location)
 		}
@@ -133,15 +158,16 @@ func parseLine(line string, location *time.Location, currentDate time.Time) (*Lo
 
 	logOpenPattern := regexp.MustCompile("^--- Log opened (.+)$")
 
-	matches := logOpenPattern.FindStringSubmatch(line)
-	if matches != nil {
-		entryTime, err := time.ParseInLocation(timeLayout, matches[1], location)
+	logOpenMatches := logOpenPattern.FindStringSubmatch(line)
+	if logOpenMatches != nil {
+		entryTime, err := time.ParseInLocation(timeLayout, logOpenMatches[1], location)
 		if err != nil {
-			return nil, fmt.Errorf("Unable to parse timestamp: %s: %s", matches[1],
+			return nil, fmt.Errorf("Unable to parse timestamp: %s: %s", logOpenMatches[1],
 				err.Error())
 		}
 
 		return &LogEntry{
+			Line:      line,
 			Time:      entryTime,
 			EntryType: LogOpen,
 		}, nil
@@ -152,17 +178,341 @@ func parseLine(line string, location *time.Location, currentDate time.Time) (*Lo
 	joinPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) \\[(\\S+?)\\] has joined (\\S+)$")
 	joinMatches := joinPattern.FindStringSubmatch(line)
 	if joinMatches != nil {
-		// TODO: Convert hour/minute into ints
-		entryTime := time.Date(currentDate.Year(), currentDate.Month(), currentDate.Day(), hour, minute, 0, 0, location)
+		entryTime, err := clockToTime(joinMatches[1], joinMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
 
 		return &LogEntry{
+			Line:      line,
 			Time:      entryTime,
 			EntryType: Join,
-			Channel:   matches[5],
-			Nick:      matches[3],
-			UserHost:  matches[4],
+			Channel:   joinMatches[5],
+			Nick:      joinMatches[3],
+			UserHost:  joinMatches[4],
+		}, nil
+	}
+
+	// Channel summary
+
+	summaryPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- Irssi: (\\S+): Total of \\d+ nicks \\[\\d+ ops, \\d+ halfops, \\d+ voices, \\d+ normal\\]$")
+
+	summaryMatches := summaryPattern.FindStringSubmatch(line)
+	if summaryMatches != nil {
+		entryTime, err := clockToTime(summaryMatches[1], summaryMatches[2],
+			currentDate, location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: ChannelSummary,
+			Channel:   summaryMatches[3],
+		}, nil
+	}
+
+	// Mode change
+
+	// TODO: Parse out the modes and who/what targeted
+
+	modePattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- mode/(\\S+) \\[.+\\] by (\\S+)$")
+
+	modeMatches := modePattern.FindStringSubmatch(line)
+	if modeMatches != nil {
+		entryTime, err := clockToTime(modeMatches[1], modeMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Mode,
+			Channel:   modeMatches[3],
+			Nick:      modeMatches[4],
+		}, nil
+	}
+
+	// Channel sync
+
+	syncPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- Irssi: Join to (\\S+) was synced in \\d+ secs$")
+
+	syncMatches := syncPattern.FindStringSubmatch(line)
+	if syncMatches != nil {
+		entryTime, err := clockToTime(syncMatches[1], syncMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: JoinSync,
+			Channel:   syncMatches[3],
+		}, nil
+	}
+
+	// Channel message
+
+	messagePattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) <(.)(\\S+)> (.+)$")
+
+	messageMatches := messagePattern.FindStringSubmatch(line)
+	if messageMatches != nil {
+		entryTime, err := clockToTime(messageMatches[1], messageMatches[2],
+			currentDate, location)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: Get channel
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Message,
+			Nick:      messageMatches[4],
+			Text:      messageMatches[5],
+		}, nil
+	}
+
+	// Quit
+
+	quitPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) \\[(\\S+)\\] has quit \\[(.*)\\]$")
+
+	quitMatches := quitPattern.FindStringSubmatch(line)
+	if quitMatches != nil {
+		entryTime, err := clockToTime(quitMatches[1], quitMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: Get channel
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Quit,
+			Nick:      quitMatches[3],
+			UserHost:  quitMatches[4],
+			Text:      quitMatches[5],
+		}, nil
+	}
+
+	// Nick change
+
+	nickPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) is now known as (\\S+)$")
+
+	nickMatches := nickPattern.FindStringSubmatch(line)
+	if nickMatches != nil {
+		entryTime, err := clockToTime(nickMatches[1], nickMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: NickChange,
+			Nick:      nickMatches[3],
+			Text:      nickMatches[4],
+		}, nil
+	}
+
+	// Day change
+
+	dayPattern := regexp.MustCompile("^--- Day changed (.+)$")
+
+	dayMatches := dayPattern.FindStringSubmatch(line)
+	if dayMatches != nil {
+		timeLayout := "Mon Jan 02 2006"
+		entryTime, err := time.ParseInLocation(timeLayout, dayMatches[1], location)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse timestamp: %s: %s", dayMatches[1],
+				err.Error())
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: DayChange,
+		}, nil
+	}
+
+	// Log closed
+
+	closePattern := regexp.MustCompile("^--- Log closed (.+)$")
+
+	closeMatches := closePattern.FindStringSubmatch(line)
+	if closeMatches != nil {
+		timeLayout := "Mon Jan 02 15:04:05 2006"
+		entryTime, err := time.ParseInLocation(timeLayout, closeMatches[1],
+			location)
+		if err != nil {
+			return nil, fmt.Errorf("Unable to parse timestamp: %s: %s",
+				closeMatches[1], err.Error())
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: LogClosed,
+		}, nil
+	}
+
+	// Now talking in
+
+	nowPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- Irssi: You are now talking in (\\S+)$")
+
+	nowMatches := nowPattern.FindStringSubmatch(line)
+	if nowMatches != nil {
+		entryTime, err := clockToTime(nowMatches[1], nowMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: NowTalking,
+			Channel:   nowMatches[3],
+		}, nil
+	}
+
+	// Channel emote
+
+	emotePattern := regexp.MustCompile("^(\\d{2}):(\\d{2})  \\* (\\S+) (.*)$")
+
+	emoteMatches := emotePattern.FindStringSubmatch(line)
+	if emoteMatches != nil {
+		entryTime, err := clockToTime(emoteMatches[1], emoteMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Emote,
+			Nick:      emoteMatches[3],
+			Text:      emoteMatches[4],
+		}, nil
+	}
+
+	// Topic change
+
+	topicPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) changed the topic of (\\S+) to: (.*)$")
+
+	topicMatches := topicPattern.FindStringSubmatch(line)
+	if topicMatches != nil {
+		entryTime, err := clockToTime(topicMatches[1], topicMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Topic,
+			Nick:      topicMatches[3],
+			Channel:   topicMatches[4],
+			Text:      topicMatches[5],
+		}, nil
+	}
+
+	// Kick
+
+	kickPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) was kicked from (\\S+) by (\\S+) \\[(.*)\\]$")
+
+	kickMatches := kickPattern.FindStringSubmatch(line)
+	if kickMatches != nil {
+		entryTime, err := clockToTime(kickMatches[1], kickMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		// TODO: 2 nicks
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Kick,
+			Nick:      kickMatches[3],
+			Channel:   kickMatches[4],
+			Text:      kickMatches[6],
+		}, nil
+	}
+
+	// Part
+
+	partPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- (\\S+) \\[(\\S+)\\] has left (\\S+) \\[(.*)\\]$")
+
+	partMatches := partPattern.FindStringSubmatch(line)
+	if partMatches != nil {
+		entryTime, err := clockToTime(partMatches[1], partMatches[2], currentDate,
+			location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: Part,
+			Nick:      partMatches[3],
+			UserHost:  partMatches[4],
+			Channel:   partMatches[5],
+			Text:      partMatches[6],
+		}, nil
+	}
+
+	// Your nick change
+
+	yourNickPattern := regexp.MustCompile("^(\\d{2}):(\\d{2}) -!- You're now known as (\\S+)$")
+
+	yourNickMatches := yourNickPattern.FindStringSubmatch(line)
+	if yourNickMatches != nil {
+		entryTime, err := clockToTime(yourNickMatches[1], yourNickMatches[2],
+			currentDate, location)
+		if err != nil {
+			return nil, err
+		}
+
+		return &LogEntry{
+			Line:      line,
+			Time:      entryTime,
+			EntryType: YourNickChange,
+			Nick:      yourNickMatches[3],
 		}, nil
 	}
 
 	return nil, fmt.Errorf("Unrecognized line: %s", line)
+}
+
+// clockToTime takes a timestamp like HH:MM and makes a time.Time type.
+// It places the HH:MM in a given date by using currentDate
+func clockToTime(hour string, minutes string, currentDate time.Time, location *time.Location) (time.Time, error) {
+	h, err := strconv.Atoi(hour)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("Unable to parse hour from timestamp: %s: %s", hour, err.Error())
+	}
+
+	m, err := strconv.Atoi(minutes)
+	if err != nil {
+		return time.Time{}, fmt.Errorf("Unable to parse minute from timestamp: %s: %s", minutes[1], err.Error())
+	}
+
+	entryTime := time.Date(currentDate.Year(), currentDate.Month(), currentDate.Day(), h, m, 0, 0, location)
+
+	return entryTime, nil
 }
